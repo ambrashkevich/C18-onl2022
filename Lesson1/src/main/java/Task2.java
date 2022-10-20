fec0fd56640fc0841c64c55db94fc6b44974da2e
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-10-20 18:19:14 +0300

    решение домашнего задания №2

---
+++ Lesson1/src/main/java/Task2.java
@@ -0,0 +1,25 @@
+public class Task2 {
+
+    public static void main(String[] args) {
+        int count = 0;
+
+       for (String arg : args) {
+
+try {
+        int i = Integer.parseInt(arg.trim());
+
+        if(i > 0){
+
+            count = count+1;
+            System.out.println(count);
+        }
+        }
+          catch (NumberFormatException nfe)
+    {
+        System.out.println("NumberFormatException: " + nfe.getMessage());
+    }
+    }
+}
+}
+
+
