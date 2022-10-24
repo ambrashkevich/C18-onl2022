b5e0bc544856cab575af7e04454748fddc2a50a5
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-10-24 21:06:15 +0300

    решение домашнего задания №2

--- Lesson1/src/main/java/Task2.java
+++ Lesson1/src/main/java/Task2.java
@@ -3,17 +3,15 @@ public class Task2 {
     public static void main(String[] args) {
         int count = 0;
         for (String arg : args) {
-            try {
-                int i = Integer.parseInt(arg.trim());
-                if (i > 0) {
-                    count = count + 1;
-                    System.out.println(count);
-                }
-            } catch (NumberFormatException nfe) {
-                System.out.println("NumberFormatException: " + nfe.getMessage());
+            int value = Integer.parseInt(arg.trim());
+            if (value > 0) {
+                count = count + 1;
             }
         }
+        System.out.println("кол-во положительных чисел" + count);
     }
 }
+
+
 
 
