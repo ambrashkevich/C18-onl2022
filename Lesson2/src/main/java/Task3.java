6e8a957c95578b234260a707989de49e963157df
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-10-26 15:31:11 +0300

    Исправление дополнительного задания(1)

--- Lesson2/src/main/java/Task3.java
+++ Lesson2/src/main/java/Task3.java
@@ -4,11 +4,15 @@ public class Task3 {
 
         int a = sum(50, 50);
         int b = sum(100, 100);
+        System.out.println(a);
+        System.out.println(b);
         System.out.println(sum(100, 200));
     }
 
     static int sum(int a, int b) {
 
         return a + b;
+
     }
+
 }
