66a0e320505bab5cd2ef950142622b9040ec7d50
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-10-26 15:22:06 +0300

    Решение дополнительного задания(1)

---
+++ Lesson2/src/main/java/Task3.java
@@ -0,0 +1,14 @@
+public class Task3 {
+
+    public static void main(String[] args) {
+
+        int a = sum(50, 50);
+        int b = sum(100, 100);
+        System.out.println(sum(100, 200));
+    }
+
+    static int sum(int a, int b) {
+
+        return a + b;
+    }
+}
