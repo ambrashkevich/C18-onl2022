4785b8714474ceef2cabb2770a7452cac91fe907
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-10-31 14:34:24 +0300

    Решение доп дз

--- Lesson2/src/main/java/Task3.java
+++ Lesson2/src/main/java/Task3.java
@@ -1,15 +1,100 @@
 public class Task3 {
-
     public static void main(String[] args) {
-
-        int a = sum(50, 50);
-        int b = sum(100, 100);
-        System.out.println(a);
-        System.out.println(b);
+        //Некоторые тесты для проверки задач.
         System.out.println(sum(100, 200));
+        System.out.println(sum(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println(max(56, 349));
+        System.out.println(average(new int[]{0, -2, 3, -1, 5}));
+        System.out.println(max(new int[]{1, 2, 3, 4, 5, 100, 99}));
+        System.out.println(calculateHypotenuse(3, 4));
     }
-    static int sum(int a, int b) {
 
-        return a + b;
+    /**
+     * 1. Метод должен вернуть сумму двух чисел a и b
+     * 2. Дополнительно: сделать проверку если сумма a и b больше чем максимальное значение int то вернуть -1
+     **/
+
+
+    public static int sum(int a, int b) {
+        if ((a + b) > Integer.MAX_VALUE) {
+            return -1;
+        } else {
+            return a + b;
+        }
+    }
+
+
+    /**
+     * Метод должен вернуть максимальное значение из двух чисел
+     *
+     * <p>
+     * Example1:
+     * a = 4,
+     * b = 5
+     * <p>
+     * Метод должен вернуть 5
+     * Example2:
+     * a = 10,
+     * b = 10
+     * <p>
+     * Метод должен вернуть 10
+     */
+    public static int max(int a, int b) {
+
+        return Math.max(a, b);
+    }
+
+
+    /**
+     * Метод должен вернуть среднее значение из массива чисел
+     * (необходимо сумму всех элеменов массива разделить на длину массива)
+     * <p>
+     * Example:
+     * array = {1,2,3,4,5}
+     * Метод должен return 3.0
+     */
+    public static double average(int[] array) {
+        double average = 0;
+        for (int i = 0; i < array.length; i++) {
+            average += array[i] / array.length;
+        }
+        return average;
+    }
+
+    /**
+     * Метод должен вернуть максимальный элемент массива. Пример: array = {1,2,10,3} метод возвращает 10
+     **/
+    public static int max(int[] array) {
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+
+    /**
+     * Используя теорему Пифагора, вычислите значение гипотенузы. Квадрат гипотенузы = сумме квадратов катетов
+     * <p>
+     * Example1:
+     * 3
+     * 4
+     * return 5
+     * <p>
+     * Example2:
+     * 12
+     * 16
+     * return 20
+     */
+
+    public static double calculateHypotenuse(int a, int b) {
+        double i = Math.sqrt(a * a + b * b);
+        return i;
     }
 }
+
+
+
+
