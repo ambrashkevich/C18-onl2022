d3f4fdc5454724d00ed2c4222759f1a2d4609e3d
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-11-10 20:20:02 +0300

    Исправление домашнего задания №4

--- Lesson4/src/main/java/Task2.java
+++ Lesson4/src/main/java/Task2.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int count = 0;
+        int count;
         System.out.print("Введите целое число:");
-        if (in.hasNext()) ;
         count = in.nextInt();
         if (count % 2 == 0) {
             System.out.println("Число" + " " + count + " " + "является чётным:");
--- Lesson4/src/main/java/Task3.java
+++ Lesson4/src/main/java/Task3.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); // создаём объект класса Scanner
-        int i = 0;
-        int a = 0;
-        int b = 0;
+        int i;
+        int a;
+        int b;
         System.out.print("Введите число: ");
         i = sc.nextInt();
         System.out.println("Введите второе число: ");
