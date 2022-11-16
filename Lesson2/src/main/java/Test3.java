cf24b385c024ab5ecfcdbd295563133ab4676d0b
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-11-16 11:54:37 +0300

    Исправление домашнего задания №4 + доп дз за 4 урок прорешено

--- Lesson2/src/main/java/Test3.java
+++ Lesson2/src/main/java/Test3.java
@@ -1,5 +1,14 @@
 public class Test3 {
     public static void main(String[] args) {
+        int[] mass = new int[5];
+        mass[2] = 10;
+        mass[4] = 11;
+        System.out.println();
+        int length = mass.length;
+        int element = mass[--length];
+        System.out.println(element);
+        System.out.println(mass[--length]);
+
 
     }
 }
--- Lesson4/src/main/java/Task5.java
+++ Lesson4/src/main/java/Task5.java
@@ -3,14 +3,14 @@ public class Task5 {
     public static void main(String[] args) {
         System.out.println("""
                            byte short char int long float double boolean
-                byte        т     я     ня   я   я     я     я       x
-                short      ня     т     ня   я   я     я     я       x
-                char       ня    ня     т    я   я     я     я       x
-                int        ня    ня     ня   т   я     я     я       x
-                long       ня    ня     ня   ня  т     я     я       x
-                float      ня    ня     ня   ня  ня    т     я       x
-                double     ня    ня     ня   ня  ня    ня    т       x
-                boolean    x      x     x    x   x     x     x       T
+                byte        т     ня    я   ня  ня   ня    ня      x
+                short       я     т     я   ня  ня   ня    ня      x
+                char        я     я     т   ня  ня   ня    ня      x
+                int         я     я     я    т  ня   ня    ня      x
+                long        я     я     я    я   т   ня    ня      x
+                float       я     я     я    я   я    т    ня      x
+                double      я     я     я    я   я    я    т       x
+                boolean     x     x     x    x   x    x    x       T
                  \s""");
     }
 
