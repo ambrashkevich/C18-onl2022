ee4cca6f4f2a755a55bd557c2b782ffed245d9ca
Вадим Амбрашкевич <vadimambr@bk.ru>
2022-11-23 16:45:37 +0300

    Дз№7

---
+++ Lesson7/src/main/java/by/tms/model/Main.java
@@ -0,0 +1,14 @@
+package by.tms.model;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer computer = new Computer("i7-9900", "HyperX 64GB", "WD Blue 2TB", 3);
+        System.out.println(computer);
+        while (computer.getResource() > 0) {
+            computer.on();
+        }
+
+
+    }
+}
+
