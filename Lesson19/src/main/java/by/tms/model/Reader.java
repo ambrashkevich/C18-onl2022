1c57cf48c5b521db4e77c6a8a4ced6011e71cf66
Вадим Амбрашкевич <vadimambr@bk.ru>
2023-01-20 01:16:31 +0300

    add homework 19(task6 I'm finishing)

--- Lesson19/src/main/java/by/tms/model/Reader.java
+++ Lesson19/src/main/java/by/tms/model/Reader.java
@@ -1,16 +1,37 @@
 package by.tms.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
+import java.util.List;
+
+
 @AllArgsConstructor
 @ToString
 public class Reader {
     private String name;
-    private String author;
-    private int theYearOfPublishing;
+    private String surname;
+    private EmailAddress email;
+    private Boolean isAgreed;
+    private List<Book> bookList;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public EmailAddress getEmail() {
+        return email;
+    }
+
+    public Boolean getAgreed() {
+        return isAgreed;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
 }
