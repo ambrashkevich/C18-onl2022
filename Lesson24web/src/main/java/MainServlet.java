acd692b76b0ef25745f8e25efd053cd0dc2214f9
Вадим Амбрашкевич <vadimambr@bk.ru>
2023-01-25 23:35:14 +0300

    web

---
+++ Lesson24web/src/main/java/MainServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("init");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+        System.out.println("service");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        System.out.println("destroy");
+    }
+}
