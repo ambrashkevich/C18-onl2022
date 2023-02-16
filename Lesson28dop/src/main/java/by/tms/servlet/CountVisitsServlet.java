4a7b9e928d743fd2d98a4d51126cf4bf8274814a
Вадим Амбрашкевич <vadimambr@bk.ru>
2023-02-16 20:53:54 +0300

    add homework 28dop, task html,css

---
+++ Lesson28dop/src/main/java/by/tms/servlet/CountVisitsServlet.java
@@ -0,0 +1,28 @@
+package by.tms.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/visit")
+public class CountVisitsServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(true);
+        Integer count = (Integer) session.getAttribute("count");
+        if (count == null) {
+            count = 1;
+        } else {
+            count++;
+        }
+        session.setAttribute("count", count);
+        resp.getWriter().println("Вы посетили сайт " + count + " times");
+    }
+}
