8746b6331614312c5eab430cfd90133e4ef12feb
Вадим Амбрашкевич <vadimambr@bk.ru>
2023-03-22 01:18:03 +0300

    spotbugs

---
+++ Lesson28dop/src/main/java/by/tms/servlet/PoemServlet.java
@@ -0,0 +1,42 @@
+package by.tms.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+//*1) Клиент при обращении к серверу получает случайно выбранное стихотворение
+// Максима Богдановича из файла.
+
+@WebServlet("/poem")
+public class PoemServlet extends HttpServlet {
+
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        try {
+            File file = new File("src/main/resources/poem");
+            if (file.canRead()) {
+                new String(getServletContext().getResourceAsStream("src/main/resources/poem" + ".txt").readAllBytes());
+            } else {
+
+            }
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/index.jsp");
+            requestDispatcher.forward(request, response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage() + "error");
+        }
+    }
+
+    public void destroy() {
+        super.destroy();
+    }
+}
